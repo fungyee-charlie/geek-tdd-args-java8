34555f2b3bb614e00153c39fedf31c79c4c4dfb7
ILoveStudyAndWork <fengyi_ling@qq.com>
2022-06-16 21:43:55 +0800

    [Fengyi]Refactor: delete useless constructor

--- src/main/java/StringParser.java
+++ src/main/java/StringParser.java
@@ -1,9 +1,5 @@
 class StringParser extends IntegerParser {
 
-    private StringParser() {
-        super(String::valueOf);
-    }
-
     public static OptionParser createStringParser() {
         return new IntegerParser(String::valueOf);
     }
