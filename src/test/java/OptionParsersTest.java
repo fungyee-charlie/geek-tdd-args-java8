878ffe59bd71c5d43aaa06ffa3f47b9546fe94da
ILoveStudyAndWork <fengyi_ling@qq.com>
2022-06-26 15:53:02 +0800

    [Fengyi]Reorganize method using @Nested

--- src/test/java/OptionParsersTest.java
+++ src/test/java/OptionParsersTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -23,48 +24,53 @@ class OptionParsersTest {
         };
     }
 
-    @Test
-    void should_return_value_when_parse_single_value_option_given_option_present() {
-        Object parsed = new Object();
-        Function<String, Object> parse = (it) -> parsed;
-        Object whatever = new Object();
-        assertEquals(parsed, OptionParsers.unary(whatever, parse)
-                .parse(asList("-p", "8080"), option("p")));
+    @Nested
+    class UnaryOptionParser {
+        @Test
+        void should_return_value_when_parse_single_value_option_given_option_present() {
+            Object parsed = new Object();
+            Function<String, Object> parse = (it) -> parsed;
+            Object whatever = new Object();
+            assertEquals(parsed, OptionParsers.unary(whatever, parse)
+                    .parse(asList("-p", "8080"), option("p")));
+        }
+
+        @Test
+        void should_not_accept_extra_argument_when_parse_single_value_option() {
+            TooManyArgumentException e = assertThrows(TooManyArgumentException.class,
+                    () -> OptionParsers.unary(0, Integer::valueOf).parse(asList("-p", "8080", "8081"), option("p")));
+            assertEquals("p", e.getOption());
+        }
+
+
+        @ParameterizedTest
+        @ValueSource(strings = {"-p -l", "-p"})
+        void should_not_accept_insufficient_argument_when_parse_single_value_option(String arguments) {
+            InsufficientArgumentException e = assertThrows(InsufficientArgumentException.class,
+                    () -> OptionParsers.unary(0, Integer::valueOf)
+                            .parse(asList(arguments.split(" ")), option("p")));
+            assertEquals("p", e.getOption());
+        }
+
+        @Test
+        void should_set_default_value_when_parse_single_value_option() {
+            Function<String, Object> parse = (it) -> null;
+            Object defaultValue = new Object();
+            assertEquals(defaultValue, OptionParsers.unary(defaultValue, parse)
+                    .parse(asList(), option("p")));
+        }
+
+        @Test
+        void should_throw_illegal_value_exception() {
+            Function<String, Object> parse = (it) -> {
+                throw new RuntimeException();
+            };
+            Object defaultValue = new Object();
+            IllegalValueException exception = assertThrows(IllegalValueException.class,
+                    () -> OptionParsers.unary(defaultValue, parse).parse(asList("-p", "list"), option("p")));
+            assertEquals("p", exception.getOption());
+        }
     }
 
-    @Test
-    void should_not_accept_extra_argument_when_parse_single_value_option() {
-        TooManyArgumentException e = assertThrows(TooManyArgumentException.class,
-                () -> OptionParsers.unary(0, Integer::valueOf).parse(asList("-p", "8080", "8081"), option("p")));
-        assertEquals("p", e.getOption());
-    }
 
-
-    @ParameterizedTest
-    @ValueSource(strings = {"-p -l", "-p"})
-    void should_not_accept_insufficient_argument_when_parse_single_value_option(String arguments) {
-        InsufficientArgumentException e = assertThrows(InsufficientArgumentException.class,
-                () -> OptionParsers.unary(0, Integer::valueOf)
-                        .parse(asList(arguments.split(" ")), option("p")));
-        assertEquals("p", e.getOption());
-    }
-
-    @Test
-    void should_set_default_value_when_parse_single_value_option() {
-        Function<String, Object> parse = (it) -> null;
-        Object defaultValue = new Object();
-        assertEquals(defaultValue, OptionParsers.unary(defaultValue, parse)
-                .parse(asList(), option("p")));
-    }
-
-    @Test
-    void should_throw_illegal_value_exception() {
-        Function<String, Object> parse = (it) -> {
-            throw new RuntimeException();
-        };
-        Object defaultValue = new Object();
-        IllegalValueException exception = assertThrows(IllegalValueException.class,
-                () -> OptionParsers.unary(defaultValue, parse).parse(asList("-p", "list"), option("p")));
-        assertEquals("p", exception.getOption());
-    }
-}
+   }
